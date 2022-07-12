bb004ca09390011d251428b8a079d13522c4f2fe
ranyery <ranyerysc@gmail.com>
2022-07-12 12:35:31 -0300

    feat: implementa logica de interacao com o teclado na diretiva keyboard-managed-item

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,24 +1,52 @@
-import { Directive, HostListener } from '@angular/core';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
+import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
+
+enum ArrowDirection {
+  LEFT = -1,
+  RIGHT = 1,
+}
 
 @Directive({ selector: '[app-keyboard-manager]' })
 export class KeyboardManagerDirective {
+  @ContentChildren(KeyboardManagedItemDirective)
+  public items!: QueryList<KeyboardManagedItemDirective>;
+
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
-        console.log('Up');
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         break;
       case 'ArrowDown':
-        console.log('Down');
+        this.moveFocus(ArrowDirection.LEFT).focus();
         break;
       case 'ArrowLeft':
-        console.log('Left');
+        this.moveFocus(ArrowDirection.LEFT).focus();
         break;
       case 'ArrowRight':
-        console.log('Right');
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         break;
       default:
         break;
     }
+  }
+
+  public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective {
+    const items = this.items.toArray();
+    const currentSelectedIndex = items.findIndex((item) => item.isFocused());
+    const targetElementFocus = items[currentSelectedIndex + direction];
+
+    if (targetElementFocus) {
+      return targetElementFocus;
+    }
+
+    return direction === ArrowDirection.LEFT
+      ? items[items.length - 1]
+      : items[0];
   }
 }
